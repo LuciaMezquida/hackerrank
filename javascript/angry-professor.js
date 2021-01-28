737a3e5e55d7818f40790a413686c395cfee442d
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-28 14:07:29 +0100

    Bulld a testing function

--- javascript/angry-professor.js
+++ javascript/angry-professor.js
@@ -1,53 +1,22 @@
 "use strict";
 
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", (inputStdin) => {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", (_) => {
-  inputString = inputString
-    .replace(/\s*$/, "")
-    .split("\n")
-    .map((str) => str.replace(/\s*$/, ""));
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 // Complete the angryProfessor function below.
-function angryProfessor(k, a) {}
+function angryProfessor(k, a) {
+  const arrivedStudents = a.filter((student) => student <= 0);
+  if (arrivedStudents.length < k) {
+    return "YES";
+  }
+  return "NO";
+}
 
 function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const k = 3;
 
-  const t = parseInt(readLine(), 10);
+  const a = [-1, -2, 1, 3, 4];
 
-  for (let tItr = 0; tItr < t; tItr++) {
-    const nk = readLine().split(" ");
-
-    const n = parseInt(nk[0], 10);
-
-    const k = parseInt(nk[1], 10);
-
-    const a = readLine()
-      .split(" ")
-      .map((aTemp) => parseInt(aTemp, 10));
-
-    let result = angryProfessor(k, a);
-
-    ws.write(result + "\n");
-  }
-
-  ws.end();
+  let result = angryProfessor(k, a);
+  if (result === "NO") {
+    console.log("ok");
+  } else if (result === "YES") console.log("not ok");
 }
+main();
