f8791d38e07dcbcbe5754cde255ce1b7e1d9b4ca
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-05 22:16:03 +0100

    Create kangaroo function and its test

---
+++ javascript/number-line-jumps.js
@@ -0,0 +1,23 @@
+"use strict";
+const samePossition = "YES";
+const wrongPossition = "NO";
+
+function kangaroo(kangaroo1, velocity1, kangaroo2, velocity2) {
+  if (
+    (kangaroo2 - kangaroo1) * (velocity2 - velocity1) < 0 &&
+    (kangaroo2 - kangaroo1) % (velocity2 - velocity1) == 0
+  ) {
+    return samePossition;
+  } else {
+    return wrongPossition;
+  }
+}
+
+function main(kangaroo1, velocity1, kangaroo2, velocity2, res) {
+  let result = kangaroo(kangaroo1, velocity1, kangaroo2, velocity2);
+  if (result === res) {
+    console.log("ok");
+  } else if (result !== res) console.log("not ok");
+}
+main(0, 2, 5, 3, wrongPossition);
+main(0, 3, 4, 2, samePossition);
