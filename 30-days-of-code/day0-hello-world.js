893eeea8f8522e65fca891b01c96284066ea7457
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-12 10:57:34 +0100

    Add hello-world function of 30 days of code challenge

---
+++ 30-days-of-code/day0-hello-world.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//https://www.hackerrank.com/challenges/30-hello-world/problem
+
+function processData(inputString) {
+  // This line of code prints the first line of output
+  console.log("Hello, World.");
+  console.log(inputString);
+  // Write the second line of output that prints the contents of 'inputString' here.
+}
