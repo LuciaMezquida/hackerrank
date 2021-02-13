c4200e503046adb751861b9b1232b8b45c41de8d
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-13 19:46:13 +0100

    Add class Polygon of 10 days of javascript challenge

---
+++ 10-days-of-javascript/day4-class.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//https://www.hackerrank.com/challenges/js10-class/problem
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  perimeter() {
+    const sumSides = this.sides.reduce((a, b) => a + b, 0);
+    return sumSides;
+  }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
