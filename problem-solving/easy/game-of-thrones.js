4cd69c22e8b9ccd74f6026b7042215f7c040adf4
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-20 16:33:22 +0100

    Solve game of thrones algorithm

--- problem-solving/easy/game-of-thrones.js
+++ problem-solving/easy/game-of-thrones.js
@@ -1,1 +1,63 @@
-function gameOfThrones(s) {}
+"use strict";
+
+//https://www.hackerrank.com/challenges/game-of-thrones/problem
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the gameOfThrones function below.
+function gameOfThrones(s) {
+  let even = 0;
+  let odd = 0;
+  const sortedString = s.toLowerCase().split("").sort();
+  while (sortedString.length > 0) {
+    const filteredString = sortedString.filter(
+      (item) => item === sortedString[0]
+    );
+    if (filteredString.length % 2 === 0) {
+      even++;
+    } else {
+      odd++;
+    }
+    sortedString.splice(0, filteredString.length);
+  }
+  if (odd > 1) {
+    return "NO";
+  } else {
+    return "YES";
+  }
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  let result = gameOfThrones(s);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
